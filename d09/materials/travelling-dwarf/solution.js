ada8d085d736e78068c616dff04da24ab15e5a48
ucsarvesh <sarvesh.narayanan@ey.com>
2017-03-21 18:34:32 -0400

    d09 homework & materials

---
+++ d09/materials/travelling-dwarf/solution.js
@@ -0,0 +1,124 @@
+console.log("WRITE YOUR SOLUTION IN HERE!");
+
+var num = 0;
+
+while (num < 4) {
+  d.move();
+  num = num + 1;
+}
+
+d.orient("down");
+
+var num = 0;
+
+while (num < 16) {
+  d.move();
+  num = num + 1;
+}
+
+d.orient("right");
+
+var num = 0;
+
+while (num < 6) {
+  d.move();
+  num = num + 1;
+}
+
+d.orient("up");
+
+var num = 0;
+
+while (num < 11) {
+  d.move();
+  num = num + 1;
+}
+
+d.orient("right");
+
+var num = 0;
+
+while (num < 6) {
+  d.move();
+  num = num + 1;
+}
+
+d.orient("down");
+
+var num = 0;
+
+while (num < 9) {
+  d.move();
+  num = num + 1;
+}
+
+d.orient("right");
+
+var num = 0;
+
+while (num < 2) {
+  d.move();
+  num = num + 1;
+}
+
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.orient("down");
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.orient("right");
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.orient("up");
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.orient("right");
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.orient("down");
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.move();
+// d.orient("right");
+// d.move();
+// d.move();
+// d.move();
